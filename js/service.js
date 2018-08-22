444c77a610dd07c2fdf15c63a865cb15e3ef8e59
Tabbatha <TabbathaRenea@gmail.com>
2018-08-22 15:19:57 -0400

    get request in service now retrieving all needed details, pulling data into controller and template. Need to add ability to watch video from search results

--- js/service.js
+++ js/service.js
@@ -8,21 +8,44 @@
 function YouTubeService($http) {
 
   let trailerStats;
-  let data;
+  let queryResults;
   let trailers = "5owigsJJCsc,LCySGDkXFpI,paREY4LLwEY,zC9b1orltc8,0B5v45HutgQ";
+  let queryIds = "";
+  let queryDetails;
 
-
-  const getYouTube = ()=> {
+  const sendQuery = (query)=> {
     return $http({
       method: 'GET',
-      url: `https://www.googleapis.com/youtube/v3/search?part=snippet&q=dogs&type=video&maxResults=4&key=AIzaSyB76Pizg-mT_itkTTAJXwJTs6_tL3KTMJk
+      url: `https://www.googleapis.com/youtube/v3/search?&q=${query}&type=video&maxResults=5&key=AIzaSyB76Pizg-mT_itkTTAJXwJTs6_tL3KTMJk&part=snippet
       `,
     }).then((response) => {
       console.log(response);
-      data = response.data.items;
-      console.log(data);
+      queryResults = response.data.items;
+      console.log(queryResults);
 
-        return data;
+      return getQueryDetails();;
+    }, (error) => {
+      console.log(error);
+    });
+  }
+
+  const getQueryDetails = () => {
+    //reset queryIds to clear previous search
+    queryIds = "";
+    //grab the id of the queryResults videos
+    for(let video of queryResults) {
+      queryIds += video.id.videoId + ",";
+    }
+
+    //request the stats and snippet for each
+    return $http({
+      method: 'GET',
+      url: `https://www.googleapis.com/youtube/v3/videos?id=${queryIds}&key=${key()}&part=statistics,snippet,player`,
+    }).then((response) => {
+      console.log(response);
+      queryDetails = response.data.items;
+      console.log(queryDetails);
+      return queryDetails;
     }, (error) => {
       console.log(error);
     });
@@ -33,14 +56,9 @@ function YouTubeService($http) {
 
     return $http({
       method: 'GET',
-      url: `https://www.googleapis.com/youtube/v3/videos?id=${trailers}&key=${key()}&part=statistics,snippet,player
-
-      `,
+      url: `https://www.googleapis.com/youtube/v3/videos?id=${trailers}&key=${key()}&part=statistics,snippet,player`,
     }).then((response) => {
       trailerStats = response;
-      console.log(trailerStats.data.items[0]);
-
-
       console.log(trailerStats);
 
       return response;
@@ -49,23 +67,14 @@ function YouTubeService($http) {
     });
   }
 
-  const addCustomTitle = () => {
-    // trailerStats.data.items[0].customTitle = "Smallfoot";
-    console.log(trailerStats);
-  }
 
 
-
-  const getStats = () => {
-      return stats;
-    }
 
 
   return {
-    getYouTube,
-    getStats,
+    sendQuery,
     getTrailers,
-    addCustomTitle
+    getQueryDetails
   }
 }
 
--- js/youtube.js
+++ js/youtube.js
@@ -1,28 +1,36 @@
 "use strict";
 
-   // <div ng-repeat="item in $ctrl.results">
-    //   <p>{{item.snippet.title}}</p>
-    //   <p>{{item.snippet.description}}</p>
-    //   <img src="{{item.snippet.thumbnails.default.url}}">
-    // </div>
+
 
 const youTube = {
   template: `
   <nav>
 
 
-    <i class="fas fa-bars menu" ></i>
+    <i class="fas fa-bars" id="menu" ></i>
 
 
 
     <a ng-repeat="trailer in $ctrl.trailers" id={{trailer.id}} ng-click="$ctrl.clickMovie($event, $index)">{{trailer.customTitle}}</a>
 
+
+
   </nav>
 
+  <form ng-submit="$ctrl.sendRequest($ctrl.query);">
+      <div class="searchWrapper">
+        <input type="text" ng-model="$ctrl.query" placeholder="Search for a movie trailer">
+        <button class="searchButton">
+          <i class="material-icons">
+          search
+          </i>
+          </button>
+      </div>
+  </form>
 
   <section class="movieInfo">
     <h2>{{$ctrl.clickedMovie.snippet.title}}</h2>
-    <p class="description" >{{$ctrl.clickedMovie.snippet.description}}</p>
+    <p class="description" >{{$ctrl.clickedMovie.snippet.description | limitTo:500 }} ...<button class="readMore">read more</button></p>
 
     <img ng-show="$ctrl.showThumb" src={{$ctrl.clickedMovie.snippet.thumbnails.medium.url}}
 
@@ -34,6 +42,16 @@ const youTube = {
 
     <iframe width="560" height="315" ng-show="$ctrl.showPlayer" src="" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
   </section>
+
+  <section class="searchResults">
+    <div ng-repeat="item in $ctrl.queryResults">
+      <p>{{item.snippet.title}}</p>
+      <p>{{item.snippet.description | limitTo:500 }} ...<button class="readMore"> read more</button></p>
+      <p>{{item.statistics.viewCount}} Views</p>
+
+      <img src="{{item.snippet.thumbnails.medium.url}}">
+    </div>
+  </section>
   `,
 
   controller: ["YouTubeService", "$timeout", function(YouTubeService, $timeout) {
@@ -41,11 +59,11 @@ const youTube = {
     const vm = this;
     vm.trailers;
     vm.clickedMovie;
+    vm.queryResults;
     vm.showPlayer = false;
     vm.showThumb = true;
     let movieInfo = document.querySelector(".movieInfo");
     let iframe = document.querySelector("iframe");
-
 
 
       YouTubeService.getTrailers().then((response) => {
@@ -55,12 +73,7 @@ const youTube = {
         vm.trailers[2].customTitle = "Hotel Transylvania";
         vm.trailers[3].customTitle = "Sherlock Gnomes";
         vm.trailers[4].customTitle = "Incredibles 2";
-
-        console.log(vm.trailers);
       })
-
-
-
 
     vm.clickMovie = ($event, $index) => {
       //fade out current data
@@ -85,7 +98,6 @@ const youTube = {
 
     vm.fadeOut = () => {
       //fade out current window
-
       angular.element(movieInfo).addClass("animated fadeOut");
       //remove fadeOut so we can add fadeIn
       $timeout( function(){
@@ -96,21 +108,19 @@ const youTube = {
     vm.watchTrailer = () => {
       vm.showPlayer = true;
       vm.showThumb = false;
-
-
     }
 
-    // YouTubeService.getYouTube().then((response) => {
-    //   console.log("this is doing something");
+    vm.sendRequest = (query) => {
+      YouTubeService.sendQuery(query).then((response) => {
 
-    //   vm.results = response;
-    //   console.log(vm.results);
+        vm.queryResults = response;
+        console.log(vm.queryResults);
 
-    // });
-
-    // vm.setStats = (id) => {
-    //   YouTubeService.setStats(id);
-    // }
+      });
+  }
+    vm.setStats = (id) => {
+      YouTubeService.setStats(id);
+    }
 
 
 
